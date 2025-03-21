9c0a3a9af5262544dbc408aa050a61435e5a04a7
RikardoAnju <rikardoanju110@gmail.com>
2025-03-21 12:28:31 +0700

    SetUp data base Firebase & Brevo

---
+++ lib/Splass_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:guide_me/home.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          "Splash Screen",
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
